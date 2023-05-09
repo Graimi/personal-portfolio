ef027a76785714257c187b26784dcb883472d763
Graimi <113901525+Graimi@users.noreply.github.com>
2023-05-09 09:19:41 +0200

    experienced data uploaded

--- src/data/Experience.js
+++ src/data/Experience.js
@@ -22,7 +22,7 @@ const experienceData = [
     job: 'Técnico de proyectos',
     date: '2020 - 2022',
     description:
-      '- Gestionar proyectos técnicos a nivel nacional <br /> - Participar en el desarrollo de proyectos sociales <br /> - Diseñar material gráfico corporativo y aplicado a los proyectos en marcha'
+      '- Beca Diputación Cáceres "Lazos que unen" y subvención "PIT" Junta de Extremadura<br /> - Gestionar proyectos técnicos a nivel nacional <br /> - Participar en el desarrollo de proyectos sociales <br /> - Diseñar material gráfico corporativo y aplicado a los proyectos en marcha'
   },
   {
     company: 'Fruit Vegetables EUROPE (EUCOFEL)',
@@ -34,18 +34,43 @@ const experienceData = [
     job: 'Asesor en Políticas EU y Comercio internacional',
     date: '2019 - 2020',
     description:
-      '- Análisis de políticas comerciales, negociaciones y acuerdos comerciales <br /> - Ayudar en las actividades de comunicación e investigación <br /> - Asistir a reuniones, conferencias y eventos'
+      '- Beca Diputación Cáceres "Lazos que unen" <br /> - Análisis de políticas comerciales, negociaciones y acuerdos comerciales <br /> - Ayudar en las actividades de comunicación e investigación <br /> - Asistir a reuniones, conferencias y eventos'
   },
   {
-    company: '',
-    logo: '',
-    link: '',
-    location: '',
-    country: '',
-    flag: '',
-    job: '',
-    date: '',
-    description: '- <br /> - <br /> -'
+    company: 'Grupo Arcor',
+    logo: 'https://res.cloudinary.com/dwsffp1eq/image/upload/v1683616042/Portfolio/Arcor_logo.svg_f9hjvm.png',
+    link: 'https://www.arcor.com/ar/',
+    location: 'Barcelona, ES',
+    country: 'España',
+    flag: 'https://res.cloudinary.com/dwsffp1eq/image/upload/v1683542606/Pa%C3%ADses/espana_pjmx7s.png',
+    job: 'Asistente del gerente de ventas de Europa',
+    date: '2018 - 2019',
+    description:
+      '- Prácticas postgrado Universidat de Barcelona <br /> - Monitorear la implementación de las acciones de ventas y los resultados <br /> - Análisis de ventas, inventarios, cadena de valor y rentabilidad <br /> - Apoyo al control del área logística de Europa'
+  },
+  {
+    company: 'Wahaca',
+    logo: 'https://res.cloudinary.com/dwsffp1eq/image/upload/v1683616344/Portfolio/wahaca_y8cnhy.png',
+    link: 'https://www.wahaca.co.uk/',
+    location: 'Brighton, UK',
+    country: 'United Kingdom',
+    flag: 'https://res.cloudinary.com/dwsffp1eq/image/upload/v1683543019/Pa%C3%ADses/reino-unido_zeybww.png',
+    job: 'Barman',
+    date: '2017 - 2018',
+    description:
+      '- Evaluar las necesidades de los clientes del bar y hacer recomendaciones <br /> - Preparación de bebidas para el bar y el restaurante <br /> - Reabastecer y reponer el inventario y los suministros del bar'
+  },
+  {
+    company: 'ASISA',
+    logo: 'https://res.cloudinary.com/dwsffp1eq/image/upload/v1683557955/Portfolio/asisa_hl0hv4.png',
+    link: 'https://www.asisa.es/seguros-de-salud',
+    location: 'Cáceres, ES',
+    country: 'España',
+    flag: 'https://res.cloudinary.com/dwsffp1eq/image/upload/v1683542606/Pa%C3%ADses/espana_pjmx7s.png',
+    job: 'Administrativo',
+    date: '2016',
+    description:
+      '- Prácticas del grado de ADE <br /> - Responder y dirigir las llamadas telefónicas <br /> - Proporcionar apoyo general a los visitantes <br /> - Proporcionar información respondiendo a preguntas y solicitudes'
   }
 ];
 
